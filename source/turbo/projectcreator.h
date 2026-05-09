3c02a87a149f30ce941d87bb9aa2d84987c42aa6
WEREMSOFT <pablo.weremczuk@gmail.com>
2026-05-09 11:05:51 +0200

    added ESC close to the glfw template

--- source/turbo/projectcreator.h
+++ source/turbo/projectcreator.h
@@ -343,40 +343,51 @@ int main(void)
         return R"(
 #include <GLFW/glfw3.h>
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
 
-    glfwTerminate();
-    return 0;
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 )";
     }
